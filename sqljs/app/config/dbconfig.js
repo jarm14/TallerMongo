5ed8e34e47545d285025c0c8f1fb2bbf218cbcc8
kellylandazuri <kellrocanlover@gmail.com>
2019-07-16 19:31:52 -0500

    Update dbconfig.js
    udate tables

--- sqljs/app/config/dbconfig.js
+++ sqljs/app/config/dbconfig.js
@@ -3,19 +3,27 @@ let sqlite3 = require('sqlite3').verbose();
 let db = new sqlite3.Database('./sqlite.db');
 
 let init = function() {
-    db.run("CREATE TABLE if not exists car (" +
-        "id INTEGER PRIMARY KEY AUTOINCREMENT," +
-        " maker TEXT," +
-        " model TEXT," +
-        " year INT," +
-        " driver INT" +
+    db.run("CREATE TABLE if not exists modelo (" +
+        "codigoMarca TEXT PRIMARY KEY ," +
+        " nombre TEXT" +
         ")");
-
+    db.run("CREATE TABLE if not exists marca (" +
+        "codigo TEXT PRIMARY KEY," +
+        " nombre TEXT" +
+        ")");
+    db.run("CREATE TABLE if not exists propietario (" +
+        "cedula TEXT PRIMARY KEY," +
+        " nombre TEXT," +
+        " fechaNacimiento TEXT" +
+        ")");
     db.run("CREATE TABLE if not exists driver (" +
-        "id INTEGER PRIMARY KEY AUTOINCREMENT," +
-        " firstName TEXT," +
-        " lastName TEXT," +
-        " car INT" +
+        "placa TEXT PRIMARY KEY  CHECK (length(placa) == 7) " +
+        " modelo TEXT," +
+        " marca TEXT," +
+        " anio INTEGER CHECK (anio>=1980 AND anio=<2019)" +
+        " transmision TEXT," +
+        " motor INTEGER," +
+        " propietario TEXT" +
         ")");
 };
 
